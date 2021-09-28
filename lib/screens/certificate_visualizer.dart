2fa9cb03b490d5890927a993de2f220d7022dddc
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-09-28 15:38:34 -0500

    cors improved

--- lib/screens/certificate_visualizer.dart
+++ lib/screens/certificate_visualizer.dart
@@ -48,9 +48,6 @@ class _CertificatesVisualizerState extends State<CertificatesVisualizer> {
     await http
         .post(
       Uri.parse(base_url + "convert_html_to_pdf"),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
       body: json.encode(
         {
           "html_base64": base64.encode(utf8.encode(html_string)),
