7f107be6ccbaf5388607c9c4dc8f0e7960e8bc7f
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-09-30 19:55:50 -0500

    build version updated

--- lib/values/version.dart
+++ lib/values/version.dart
@@ -2,5 +2,5 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 Future<String> current_software_version() async {
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
-  return "Xapptor 3.0.6 - ${packageInfo.appName} ${packageInfo.version}";
+  return "Xapptor 3.0.7 - ${packageInfo.appName} ${packageInfo.version}";
 }
