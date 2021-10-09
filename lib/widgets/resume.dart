8ccadea9df18d7bc9021ec399f33920c074305dc
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2021-10-09 05:15:54 -0500

    resume improved

--- lib/widgets/resume.dart
+++ lib/widgets/resume.dart
@@ -14,9 +14,6 @@ resume({
   bool portrait = screen_height > screen_width;
 
   List<Widget> jobs = [];
-
-  double resume_section_width = screen_width * 0.65;
-
   resume.jobs.forEach((job) {
     jobs.add(
       resume_section(
@@ -27,7 +24,6 @@ resume({
   });
 
   List<Widget> sections = [];
-
   resume.sections.forEach((section) {
     sections.add(
       resume_section(
@@ -37,115 +33,133 @@ resume({
     );
   });
 
-  return Container(
-      margin: EdgeInsets.symmetric(vertical: screen_height / 6),
-      //color: Colors.orange,
-      child: Row(
-        children: [
-          Spacer(flex: 1),
-          Expanded(
-            flex: portrait ? 12 : 2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: screen_width / (portrait ? 2 : 6),
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  //color: Colors.lightBlue,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          margin: EdgeInsets.only(right: 20),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(
-                              20,
-                            ),
-                            child: Image.asset(
-                              resume.image_src,
-                              fit: BoxFit.contain,
-                            ),
+  Widget resume_body = Container(
+    width: screen_width,
+    //color: Colors.orange,
+    child: Row(
+      children: [
+        Spacer(flex: 1),
+        Expanded(
+          flex: portrait ? 12 : 2,
+          child: Column(
+            children: [
+              Container(
+                height: screen_width / (portrait ? 2 : 5),
+                margin: EdgeInsets.symmetric(vertical: 20),
+                //color: Colors.lightBlue,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(right: 20),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                            20,
+                          ),
+                          child: Image.asset(
+                            resume.image_src,
+                            fit: BoxFit.contain,
                           ),
                         ),
                       ),
-                      Expanded(
-                        flex: 3,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              child: Text(
-                                resume.name,
-                                textAlign: TextAlign.left,
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: portrait ? 22 : 28,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              resume.job_title,
-                              textAlign: TextAlign.left,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            child: Text(
+                              resume.name,
+                              textAlign: TextAlign.right,
                               style: TextStyle(
                                 color: Colors.black,
-                                fontSize: 18,
+                                fontSize: portrait ? 18 : 24,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                          Text(
+                            resume.job_title,
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: portrait ? 16 : 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  //color: Colors.greenAccent,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      resume_section(
-                        resume_section: ResumeSection(
-                          icon: Icons.account_circle_rounded,
-                          title: "Profile",
-                          description: resume.profile,
-                        ),
-                        context: context,
+              ),
+              Container(
+                //color: Colors.greenAccent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    resume_section(
+                      resume_section: ResumeSection(
+                        icon: Icons.account_circle_rounded,
+                        title: "Profile",
+                        description: resume.profile,
                       ),
-                      resume_section(
-                        resume_section: ResumeSection(
-                          icon: Icons.account_circle_rounded,
-                          title: "Details",
-                          subtitle: (resume.address ?? "") +
-                              " " +
-                              (resume.phone_number ?? ""),
-                          description: resume.email,
-                        ),
-                        context: context,
+                      context: context,
+                    ),
+                    resume_section(
+                      resume_section: ResumeSection(
+                        icon: Icons.account_circle_rounded,
+                        title: "Details",
+                        subtitle: (resume.address ?? "") +
+                            " " +
+                            (resume.phone_number ?? ""),
+                        description: resume.email,
                       ),
-                      Column(
-                        children: jobs,
-                      ),
-                      Column(
-                        children: sections,
-                      ),
-                    ],
-                  ),
+                      context: context,
+                    ),
+                    Column(
+                      children: jobs,
+                    ),
+                    Column(
+                      children: sections,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Spacer(flex: 1),
-        ],
-      ));
+        ),
+        Spacer(flex: 1),
+      ],
+    ),
+  );
+
+  return Container(
+    height: screen_height * (portrait ? 2.8 : 1.5),
+    //color: Colors.cyan,
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 1000),
+          curve: Curves.easeInOut,
+          left: visible ? 0 : screen_width,
+          child: AnimatedOpacity(
+            opacity: visible ? 1 : 0,
+            duration: Duration(milliseconds: visible ? 1000 : 500),
+            child: resume_body,
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 resume_section({
   required ResumeSection resume_section,
-  //required double width,
   required BuildContext context,
 }) {
   double screen_height = MediaQuery.of(context).size.height;
