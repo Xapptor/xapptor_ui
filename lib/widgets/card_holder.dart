eaa066fb0dfe08dc7641f786909ef689ca9ea043
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2022-06-21 01:57:50 -0500

    card holder improved

--- lib/widgets/card_holder.dart
+++ lib/widgets/card_holder.dart
@@ -17,6 +17,8 @@ class CardHolder extends StatefulWidget {
     this.elevation = 3,
     this.border_radius = 20,
     this.is_focused = false,
+    this.has_delete_button = false,
+    this.has_edit_button = false,
   });
 
   Function() on_pressed;
@@ -31,6 +33,8 @@ class CardHolder extends StatefulWidget {
   double elevation;
   double border_radius;
   bool is_focused;
+  bool has_delete_button;
+  bool has_edit_button;
 
   @override
   _CardHolderState createState() => _CardHolderState();
@@ -50,127 +54,151 @@ class _CardHolderState extends State<CardHolder> {
     double text_padding = MediaQuery.of(context).size.height / 40;
     bool portrait = is_portrait(context);
 
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return MouseRegion(
-          onEnter: (PointerEvent details) {
-            size_multiplier = 1;
-            widget.is_focused = true;
-            setState(() {});
-          },
-          onExit: (PointerEvent details) {
-            size_multiplier = 0.85;
-            widget.is_focused = false;
-            setState(() {});
-          },
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                AnimatedContainer(
-                  duration: animation_duration,
-                  height: constraints.maxHeight *
-                      (widget.is_focused ? 1 : size_multiplier),
-                  width: constraints.maxWidth *
-                      (widget.is_focused ? 1 : size_multiplier),
-                  child: CustomCard(
-                    animation_duration: animation_duration,
-                    on_pressed: widget.on_pressed,
-                    elevation: widget.is_focused ? 12 : widget.elevation,
-                    border_radius: widget.border_radius,
-                    child: Container(
-                      padding: EdgeInsets.only(
-                        bottom: text_padding,
-                      ),
-                      child: FractionallySizedBox(
-                        widthFactor: 0.85,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            AutoSizeText(
-                              widget.title ?? "",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: widget.text_color,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              minFontSize: 18,
-                              maxLines: 1,
-                              overflow: TextOverflow.visible,
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return MouseRegion(
+              onEnter: (PointerEvent details) {
+                size_multiplier = 1;
+                widget.is_focused = true;
+                setState(() {});
+              },
+              onExit: (PointerEvent details) {
+                size_multiplier = 0.85;
+                widget.is_focused = false;
+                setState(() {});
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    AnimatedContainer(
+                      duration: animation_duration,
+                      height: constraints.maxHeight *
+                          (widget.is_focused ? 1 : size_multiplier),
+                      width: constraints.maxWidth *
+                          (widget.is_focused ? 1 : size_multiplier),
+                      child: CustomCard(
+                        animation_duration: animation_duration,
+                        on_pressed: widget.on_pressed,
+                        elevation: widget.is_focused ? 12 : widget.elevation,
+                        border_radius: widget.border_radius,
+                        child: Container(
+                          padding: EdgeInsets.only(
+                            bottom: text_padding,
+                          ),
+                          child: FractionallySizedBox(
+                            widthFactor: 0.85,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                AutoSizeText(
+                                  widget.title ?? "",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    color: widget.text_color,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  minFontSize: 18,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.visible,
+                                ),
+                                AutoSizeText(
+                                  widget.subtitle,
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    color: widget.text_color,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  minFontSize: 12,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.visible,
+                                ),
+                              ],
                             ),
-                            AutoSizeText(
-                              widget.subtitle,
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: widget.text_color,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              minFontSize: 12,
-                              maxLines: 1,
-                              overflow: TextOverflow.visible,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: widget.title == null && widget.subtitle.isEmpty
-                      ? Alignment.center
-                      : Alignment.topCenter,
-                  child: AnimatedContainer(
-                    height: widget.title == null && widget.subtitle.isEmpty
-                        ? (constraints.maxHeight *
-                            (widget.is_focused ? 1 : size_multiplier))
-                        : (constraints.maxHeight / (portrait ? 2.2 : 1.8)),
-                    width: constraints.maxWidth *
-                        (widget.is_focused ? 1 : size_multiplier),
-                    duration: animation_duration,
-                    curve: Curves.linear,
-                    child: CustomCard(
-                      on_pressed: widget.on_pressed,
-                      elevation: 0,
-                      border_radius: widget.border_radius,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: widget.icon_background_color,
-                          borderRadius: BorderRadius.circular(
-                            widget.border_radius,
                           ),
                         ),
-                        child: widget.icon != null
-                            ? Icon(
-                                widget.icon,
-                                color: Colors.white.withOpacity(1.0),
-                                size: MediaQuery.of(context).size.height / 20,
-                              )
-                            : widget.image_src != ""
-                                ? ClipRRect(
-                                    borderRadius: BorderRadius.circular(
-                                      widget.border_radius,
-                                    ),
-                                    child: Image.asset(
-                                      widget.image_src,
-                                      fit: widget.image_fit,
-                                      alignment:
-                                          widget.background_image_alignment,
-                                    ),
-                                  )
-                                : Container(),
                       ),
                     ),
-                  ),
+                    Align(
+                      alignment: widget.title == null && widget.subtitle.isEmpty
+                          ? Alignment.center
+                          : Alignment.topCenter,
+                      child: AnimatedContainer(
+                        height: widget.title == null && widget.subtitle.isEmpty
+                            ? (constraints.maxHeight *
+                                (widget.is_focused ? 1 : size_multiplier))
+                            : (constraints.maxHeight / (portrait ? 2.2 : 1.8)),
+                        width: constraints.maxWidth *
+                            (widget.is_focused ? 1 : size_multiplier),
+                        duration: animation_duration,
+                        curve: Curves.linear,
+                        child: CustomCard(
+                          on_pressed: widget.on_pressed,
+                          elevation: 0,
+                          border_radius: widget.border_radius,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: widget.icon_background_color,
+                              borderRadius: BorderRadius.circular(
+                                widget.border_radius,
+                              ),
+                            ),
+                            child: widget.icon != null
+                                ? Icon(
+                                    widget.icon,
+                                    color: Colors.white.withOpacity(1.0),
+                                    size:
+                                        MediaQuery.of(context).size.height / 20,
+                                  )
+                                : widget.image_src != ""
+                                    ? ClipRRect(
+                                        borderRadius: BorderRadius.circular(
+                                          widget.border_radius,
+                                        ),
+                                        child: Image.asset(
+                                          widget.image_src,
+                                          fit: widget.image_fit,
+                                          alignment:
+                                              widget.background_image_alignment,
+                                        ),
+                                      )
+                                    : Container(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            );
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
-        );
-      },
+          onPressed: () {
+            //
+          },
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            //
+          },
+        ),
+      ],
     );
   }
 }
