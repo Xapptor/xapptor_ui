3759e601df9959a88e23fb33915a2fbcc9386e46
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-08-21 09:47:47 -0600

    Feat: ignore_for_file: use_build_context_synchronously

--- lib/widgets/contact_us_container/contact_us_container_lead_form.dart
+++ lib/widgets/contact_us_container/contact_us_container_lead_form.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_typing_uninitialized_variables, use_build_context_synchronously
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
--- lib/widgets/contact_us_container/send_button.dart
+++ lib/widgets/contact_us_container/send_button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:xapptor_ui/widgets/contact_us_container/contact_us_container.dart';
